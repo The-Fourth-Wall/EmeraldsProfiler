22c55a9688c4504a45d2ca0e0733c3e47c205621
oblivious <atha@fourth-wall.org>
2025-03-05 17:39:07 +0200

    [master] - Updated to latest version of the Emeralds package manager.

--- spec/EmeraldsProfiler.spec.c
+++ spec/EmeraldsProfiler.spec.c
@@ -1,6 +1,6 @@
 #include "../libs/cSpec/export/cSpec.h"
 #include "profiler_base/profiler_base.module.spec.h"
 
-spec_suite({ T_profiler_base(); });
-
-int main(void) { run_spec_suite("all"); }
+int main(void) {
+  cspec_run_suite("all", { T_profiler_base(); });
+}
